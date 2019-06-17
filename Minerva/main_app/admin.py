994b31ff3e5ebe3fde82985f77095c1a75e9e443
flubbid <brendanwilson1@gmail.com>
2019-06-17 17:46:18 -0500

    registers user, student, teacher, parent

--- Minerva/main_app/admin.py
+++ Minerva/main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Student, Teacher, Parent
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Parent)
+
