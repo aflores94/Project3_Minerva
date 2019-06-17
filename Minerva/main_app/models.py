e08750726361236085d7b690518139107198fe14
flubbid <brendanwilson1@gmail.com>
2019-06-17 17:47:32 -0500

    updates models

--- Minerva/main_app/models.py
+++ Minerva/main_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+
+class User(AbstractUser):
+    is_student = models.BooleanField('student status', default=False)
+    is_teacher = models.BooleanField('teacher status', default=False)
+    is_parent = models.BooleanField('parent status', default=False)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Teacher(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    # phone_number = models.IntegerField(min_length=10)
+
+class Parent(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    # phone_number = models.IntegerField(min_length=10)
+
+
+class Assignment(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='assignments')
+    due_date = models.DateField()
+    description = models.TextField(max_length=300)
+    title = models.CharField(max_length=50)
+    document = models.FileField()
+
