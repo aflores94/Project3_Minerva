f04a2d0d58e5f18ff281dc49da7567a9790b0c33
flubbid <brendanwilson1@gmail.com>
2019-06-17 10:32:26 -0500

    adds student signup view

---
+++ Minerva/main_app/views/student.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, ListView, UpdateView
+
+from ..forms import StudentSignUpForm
+from ..models import Student, User
+
+class StudentSignUpView(CreateView):
+    model = User
+    form_class = StudentSignUpForm
+    template_name='registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'student'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(#this needs to link to the student home page)
