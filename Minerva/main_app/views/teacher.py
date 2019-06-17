c864968692592a66bf2b9e5d5758f386ceff2113
flubbid <brendanwilson1@gmail.com>
2019-06-17 12:58:50 -0500

    Adds teacher signup view

--- Minerva/main_app/views/teacher.py
+++ Minerva/main_app/views/teacher.py
@@ -7,3 +7,17 @@ from django.views.generic import CreateView, ListView, UpdateView
 from ..forms import TeacherSignUpForm
 from ..models import Teacher, User
 
+
+class TeacherSignUpView(CreateView):
+    model = User
+    form_class = TeacherSignUpForm
+    template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'teacher'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('Teachers/index.html')
