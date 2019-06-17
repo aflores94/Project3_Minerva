38e4999756c89140774a4093a486f84e58efd009
flubbid <brendanwilson1@gmail.com>
2019-06-17 17:46:59 -0500

    updates forms to add data to user

--- Minerva/main_app/forms.py
+++ Minerva/main_app/forms.py
@@ -1,47 +1,40 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
+from django.db import transaction
 from .models import User, Student, Teacher, Parent
 
 class TeacherSignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
-    email = forms.EmailField()
-
     class Meta(UserCreationForm.Meta):
         model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
 
-    def save(self, commit=True):
+    @transaction.atomic
+    def save(self):
         user = super().save(commit=False)
-        user.is_teacher = True
-        if commit:
-            user.save()
+        user.is_parent = True
+        user.save()
         return user
 
 class StudentSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        first_name = forms.CharField(max_length=30)
-        last_name = forms.CharField(max_length=30)
-        email = forms.EmailField()
+        fields = ('username', 'first_name', 'last_name', 'email')
 
-    def save(self, commit=True):
+    @transaction.atomic
+    def save(self):
         user = super().save(commit=False)
-        user.is_student = True
-        if commit:
-            user.save()
+        user.is_parent = True
+        user.save()
         return user
 
 class ParentSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        first_name = forms.CharField(max_length=30)
-        last_name = forms.CharField(max_length=30)
-        email = forms.EmailField()
+        fields = ('username', 'first_name', 'last_name', 'email')
 
-    def save(self, commit=True):
+    @transaction.atomic
+    def save(self):
         user = super().save(commit=False)
         user.is_parent = True
-        if commit:
-            user.save()
+        user.save()
         return user
