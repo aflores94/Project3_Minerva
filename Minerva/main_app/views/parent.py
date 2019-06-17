058bc31633027c1bb2e65cb9d3c84d0348ba8e43
flubbid <brendanwilson1@gmail.com>
2019-06-17 12:56:18 -0500

    fixes double import error

--- Minerva/main_app/views/parent.py
+++ Minerva/main_app/views/parent.py
@@ -7,16 +7,6 @@ from django.views.generic import CreateView, ListView, UpdateView
 from ..forms import ParentSignUpForm
 from ..models import Parent, User
 
-from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect, render
-from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, ListView, UpdateView
-
-from ..forms import ParentSignUpForm
-from ..models import Parent, User
-
-
 class ParentSignUpView(CreateView):
     model = User
     form_class = ParentSignUpForm
