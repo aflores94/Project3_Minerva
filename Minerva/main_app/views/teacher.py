f4802e98700da9de39d7b2ab8dec1a24095b54d4
flubbid <brendanwilson1@gmail.com>
2019-06-17 12:57:17 -0500

    adds teacher view file

---
+++ Minerva/main_app/views/teacher.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, ListView, UpdateView
+
+from ..forms import TeacherSignUpForm
+from ..models import Teacher, User
+
