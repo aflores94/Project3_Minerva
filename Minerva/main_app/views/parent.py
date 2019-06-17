568dcb85f286955df8145f84438090f781599a41
flubbid <brendanwilson1@gmail.com>
2019-06-17 12:44:05 -0500

    adds parent signupview

--- Minerva/main_app/views/parent.py
+++ Minerva/main_app/views/parent.py
@@ -7,5 +7,22 @@ from django.views.generic import CreateView, ListView, UpdateView
 from ..forms import ParentSignUpForm
 from ..models import Parent, User
 
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, ListView, UpdateView
 
+from ..forms import ParentSignUpForm
+from ..models import Parent, User
+
+
+class ParentSignUpView(CreateView):
+    model = User
+    form_class = ParentSignUpForm
+    template_name = 'registration/signup_form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'parent'
+        return super().get_context_data(**kwargs)
 
