7a05c0715bbc391674c9b788cb7a2cae88de3bbf
flubbid <brendanwilson1@gmail.com>
2019-06-17 17:55:18 -0500

    adds views

--- Minerva/main_app/urls.py
+++ Minerva/main_app/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path 
-from . import views
+from .views import main, student, parent, teacher
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('', main.home, name='home'),
+
+    path('Students/home.html', student.login, name='login'),
+    path('Students/index.html', student.index, name='student_index'),
+
+    path('Teachers/home.html', teacher.login, name='login'),
+    path('Teachers/index.html', teacher.index, name='teacher_index'),
+
+    path('Parents/home.html', parent.login, name='login'),
+    path('Parents/index.html', parent.index, name='parent_index'),
 ]
