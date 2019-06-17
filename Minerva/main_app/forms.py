24fc764d71b19a2571f9d91bf267fed0339264ec
flubbid <brendanwilson1@gmail.com>
2019-06-17 12:45:41 -0500

    adds forms for student,parent,teacher signup

--- Minerva/main_app/forms.py
+++ Minerva/main_app/forms.py
@@ -1,2 +1,39 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+
+from .models import User, Student, Teacher, Parent
+
+class TeacherSignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_teacher = True
+        if commit:
+            user.save()
+        return user
+
+class StudentSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_student = True
+        if commit:
+            user.save()
+        return user
+
+class ParentSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_parent = True
+        if commit:
+            user.save()
+        return user
