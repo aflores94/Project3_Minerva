c27efe0d859ed8934c3d9e053af56f2dc2a947a6
flubbid <brendanwilson1@gmail.com>
2019-06-18 11:39:18 -0500

    fixes reference to .html documents to the correct paths

--- Minerva/main_app/urls.py
+++ Minerva/main_app/urls.py
@@ -4,14 +4,14 @@ from .views import teacher, student, parent, main
 urlpatterns = [
     path('', main.home, name='home'),
 
-    path('Students/home.html', student.login, name='login'),
-    path('Students/index.html', student.index, name='student_index'),
+    path('Students/home/', student.login, name='login'),
+    path('Students/index/', student.index, name='student_index'),
 
-    path('Teachers/home.html', teacher.login, name='login'),
-    path('Teachers/index.html', teacher.index, name='teacher_index'),
+    path('Teachers/home/', teacher.login, name='login'),
+    path('Teachers/index/', teacher.index, name='teacher_index'),
 
-    path('Parents/home.html', parent.login, name='login'),
-    path('Parents/index.html', parent.index, name='parent_index'),
+    path('Parents/home/', parent.login, name='login'),
+    path('Parents/index/', parent.index, name='parent_index'),
 ]
 
 
