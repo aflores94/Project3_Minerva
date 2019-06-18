3bf844edd9f119921988fa01f665adffc6e9031a
flubbid <brendanwilson1@gmail.com>
2019-06-18 11:01:49 -0500

    adds view files from ogiringal dfocumtaucnt

--- Minerva/main_app/views/main.py
+++ Minerva/main_app/views/main.py
@@ -17,3 +17,42 @@ def home(request):
     return render(request, 'home.html')
 
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+
+    form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def login_view(request):
+    error_message = ''
+    if request.method == 'POST':
+        next = request.POST.get('next') or None
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None and next is not None:
+                login(request, user)
+                return redirect(next)
+            elif user is not None and next is None:
+                login(request, user)
+                return redirect('home')
+            else:
+                error_message = 'Sorry, your username or password was invalid'
+    form = LoginForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
